bc739ea597b9519fa772063a4edf1f991b921f2d
n.danilyuk <n.danilyuk@integrasources.com>
2025-12-25 15:38:46 +0700

    The script for building on Windows has been improved.

--- scripts/deploy/build_app_windows.bat
+++ scripts/deploy/build_app_windows.bat
@@ -1,15 +1,8 @@
 
-set CONDA_DIR=%cd%\conda
-
 for /F %%i in ('git rev-parse --short HEAD') do set GIT_COMMIT_SHORT=%%i
 set LINUX_VER="3.00"
-set YEAR=%date:~-4%
-set MONTH=%date:~3,2%
-set DATE=%YEAR%-%MONTH%
-call conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
-call conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 
-call conda env create -f ..\..\environment_uni_pyrpl.yml
+call conda env create -f ..\..\environment_pyrpl_windows.yml
 call conda activate pyrpl-env
 
 cd ..\..
@@ -18,4 +11,4 @@ python setup.py develop
 
 pyinstaller pyrpl.spec
 
-xcopy /f .\dist\pyrpl.exe .\pyrpl-windows-%DATE-%LINUX_VER-%GIT_COMMIT_SHORT%.exe
+xcopy /Y .\dist\pyrpl.exe .\pyrpl-windows-%LINUX_VER%-%GIT_COMMIT_SHORT%.exe 
